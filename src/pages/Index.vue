1af6eae98009c7b6130d9475c833c586b95a7071
James Ashford <james.ashford@hotmail.com>
2020-05-13 18:21:47 +0100

    init
    Quasar CLI

---
+++ src/pages/Index.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-page padding>
+    <input 
+      v-model="message" 
+      @keyup="handleKeyup" 
+    />
+    <button @click="clearMessage">Clear</button>
+    <h5>{{message}}</h5>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "hello quasar"
+    };
+  },
+  methods: {
+    clearMessage() {
+      this.message = "";
+    },
+    handleKeyup(e) {
+      if (e.keyCode == 27) {
+        this.clearMessage();
+      } else if (e.keyCode == 13) {
+        this.alertMessage();
+      }
+    },
+    alertMessage() {
+      alert(this.message);
+    }
+  }
+};
+</script>
